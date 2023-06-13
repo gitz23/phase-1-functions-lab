ecd53d849c0275e2cfb28e7b15f62581879ef808
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-06-13 15:53:38 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+   if (someValue > 42){
+    let result = someValue - 42;
+   return result;
+}else if (someValue < 42){
+    let result = 42 -someValue;
+    return result;
+}
+}
+
+function distanceFromHqInFeet(someValue){
+    distanceFromHqInBlocks(someValue)
+    return result * 264;
+  
+}
