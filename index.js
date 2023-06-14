1e498398fbc95c9cfeab0ff8cd4e573abec8bf83
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-06-14 14:14:50 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -9,8 +9,33 @@ function distanceFromHqInBlocks(someValue){
 }
 }
 
-function distanceFromHqInFeet(someValue){
-    distanceFromHqInBlocks(someValue)
-    return result * 264;
-  
+function distanceFromHqInFeet(value){
+    let result = distanceFromHqInBlocks(value) * 264
+    return result;
+}
+
+function distanceTravelledInFeet(start, destination){
+    if (destination > start){
+        let result = (destination - start) * 264;
+        return result;
+    }else if (destination < start){
+        let result = (start - destination) * 264;
+        return result;
+    }
+}
+
+function calculatesFarePrice(start, destination){ 
+    let result = distanceTravelledInFeet(start, destination)
+    if (result < 400){
+        return 0
+    }
+    else if(result > 400 && result < 2000){
+        return (result - 400) * 0.02
+    }
+    else if(result > 2000 && result<= 2500){
+        return (result-400) * .25
+    }
+    else{
+        return 'cannot travel that far'
+    }
 }
